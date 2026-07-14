3823c6d3154dd50e4d9e338a594ba9a46b5f135f
Захар <125126828+Zaldrik@users.noreply.github.com>
2026-07-14 18:28:31 +0300

    Updates: -testing Pathlib + datetime -firt prototype of scaning files - u nigga btw >_<

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+#here u NIGGA put everything what we PROBABLY will use to done settings
+#everything changeable
 down_path = Path(r"D:\test_downloads")
 dir_path = Path(r"D:\test_downloads")
 photo = [".png", ".jpg",]
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from pathlib import Path
 from config import *
 from datetime import datetime
 
-
+#get curent day
 curent_time = datetime.now()
 print(curent_time)
 
+#get all files into dir + detect whether it is photo or not (just testing for now)
 for x in down_path.iterdir():
     if x.suffix in photo:
         print(x)
@@ -15,7 +16,7 @@ for x in down_path.iterdir():
 
 
 
-
+#creating new dir named photos if it wasnt created b4
 new_dir = dir_path / "photos"
 new_dir.mkdir(mode=0o777, parents=True, exist_ok=True)
 
