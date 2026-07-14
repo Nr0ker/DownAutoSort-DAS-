2fbfb949de826d56d6d793760d2ff1a4d6817daf
Захар <125126828+Zaldrik@users.noreply.github.com>
2026-07-14 18:24:02 +0300

    Updates: -print hello world added

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+down_path = Path(r"D:\test_downloads")
+dir_path = Path(r"D:\test_downloads")
+photo = [".png", ".jpg",]
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('hello world')
+from pathlib import Path
+from config import *
+from datetime import datetime
+
+
+curent_time = datetime.now()
+print(curent_time)
+
+for x in down_path.iterdir():
+    if x.suffix in photo:
+        print(x)
+        cr_date = datetime.fromtimestamp(x.stat().st_ctime)
+        print(cr_date)
+        print(" ")
+
+
+
+
+new_dir = dir_path / "photos"
+new_dir.mkdir(mode=0o777, parents=True, exist_ok=True)
+
+
